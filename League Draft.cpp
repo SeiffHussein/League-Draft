3eaba288e03a6fa34eae052eb316a2758e527f12
Seif Hussein <b00097215@aus.edu>
2024-06-02 21:23:57 +0400

    Basic attributes, display methods and basic main done.

--- League Draft.cpp
+++ League Draft.cpp
@@ -17,6 +17,10 @@ public:
     Club(const string & clubname, const string stadium, int capacity, float rating)
     :m_clubname(clubname),m_stadium(stadium),m_capacity(capacity),m_rating(rating){}
 
+    void  display() const {
+        cout << m_clubname << endl;
+    }
+
 
 
 };
@@ -28,12 +32,36 @@ private:
     int club_no;
     vector<Club> clubs;
 public: 
+    League(string const name) : League_name(name), club_no(0) {}
     void addclub(Club const & club) {
         clubs.push_back(club);
+        club_no = clubs.size(); //need something more efficient O(n)
     }
-    void setClubNo(int n) { club_no = n; }
+    int getClubno() { return club_no; }
+    void displayLeague()const {
+        cout << "League name: " << League_name << endl;
+        cout << "Number of clubs: " << club_no << endl; 
+        cout << "clubs:\n";
+        for (const auto & Club : clubs) {
+            Club.display();
+
+    }
+                        }
+
 };
 
 
 
-int main() {}
+int main() {
+    League premier_league("Premier League");
+    Club club1("Manchester United", "Old Trafford", 74000, 84.5);
+    Club club2("Liverpool", "Anfield", 54000, 94.7);
+    Club club3("Chelsea", "Stamford Bridge", 42000, 88.3);
+    Club club4("Arsenal", "Emirates Stadium", 60260, 90.4);
+    Club club5("Manchester City", "Etihad Stadium", 55000, 95.6);
+    vector<Club> clubs = { club1,club2,club3,club4,club5 };
+    for (auto club : clubs) {
+        premier_league.addclub(club);
+    }
+    premier_league.displayLeague();
+}
