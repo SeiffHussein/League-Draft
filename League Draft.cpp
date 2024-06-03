d71ddc7dabe199e013b84f49806aa379e2eeab0f
Seif Hussein <b00097215@aus.edu>
2024-06-03 04:24:49 +0400

    Matchday Creation done

--- League Draft.cpp
+++ League Draft.cpp
@@ -5,6 +5,9 @@
 #include <algorithm>
 #include <vector>
 #include <ctime>
+#include <cstdlib>
+#include <random>
+#include <utility>
 using namespace std;
 
 class Club {
@@ -19,8 +22,10 @@ public:
 
     void  display() const {
         cout << m_clubname << '\n' << m_stadium << " Stadium\n" 
-            << "Capicity: " << m_capacity << endl;
+            << "Capicity: " << m_capacity << endl << "\n\n";
     }
+
+    string getclubname() { return m_clubname; }
 
 
 
@@ -32,11 +37,25 @@ private:
     string League_name;
     int club_no;
     vector<Club> clubs;
+    vector <pair<Club&, Club&>> matchday;
+
 public: 
     League(string const name) : League_name(name), club_no(0) {}
     void addclub(Club const & club) {
         clubs.push_back(club);
         club_no = clubs.size(); //need something more efficient O(n)
+    }
+    void addmatchday() {
+        vector<int> indices(club_no);
+        for (int i = 0; i < club_no; ++i) {
+            indices[i] = i;
+        }
+        random_shuffle(indices.begin(), indices.end());
+
+        for (int i = 0; i < club_no; i += 2) {
+            matchday.push_back({ clubs[indices[i]], clubs[indices[i + 1]] });
+        }
+
     }
     int getClubno() { return club_no; }
     void displayLeague()const {
@@ -48,21 +67,47 @@ public:
 
     }
                         }
-
+    void displayMatchday() {
+        for (auto itr = matchday.begin(); itr != matchday.end(); itr++) {
+            cout << itr->first.getclubname() << " vs " << itr->second.getclubname() << endl;
+        }
+    }
 };
 
 
 
 int main() {
+    int x = time(0);
+    srand(x);
     League premier_league("Premier League");
     Club club1("Manchester United", "Old Trafford", 74000, 84.5);
     Club club2("Liverpool", "Anfield", 54000, 94.7);
     Club club3("Chelsea", "Stamford Bridge", 42000, 88.3);
     Club club4("Arsenal", "Emirates", 60260, 90.4);
     Club club5("Manchester City", "Etihad", 55000, 95.6);
-    vector<Club> clubs = { club1,club2,club3,club4,club5 };
+    Club club6("Tottenham Spurs", "White Heartlane", 30000, 86.1);
+    Club club7("Leicester City", "King Power Stadium", 32312, 82.1);
+    Club club8("West Ham United", "London Stadium", 60000, 78.9);
+    Club club9("Everton", "Goodison Park", 39209, 81.4);
+    Club club10("Aston Villa", "Villa Park", 42785, 79.8);
+    Club club11("Wolverhampton Wanderers", "Molineux Stadium", 32050, 77.6);
+    Club club12("Leeds United", "Elland Road", 37966, 75.2);
+    Club club13("Crystal Palace", "Selhurst Park", 25486, 72.9);
+    Club club14("Southampton", "St Mary's Stadium", 32389, 74.3);
+    Club club15("Brighton & Hove Albion", "Amex Stadium", 30750, 71.5);
+    Club club16("Newcastle United", "St James' Park", 52389, 73.8);
+    Club club17("Burnley", "Turf Moor", 21401, 69.2);
+    Club club18("Fulham", "Craven Cottage", 19359, 67.4);
+    Club club19("Sheffield United", "Bramall Lane", 32702, 68.7);
+    Club club20("West Bromwich Albion", "The Hawthorns", 26688, 66.9);
+    vector<Club> clubs = { club1, club2, club3, club4, club5, club6, club7, club8, club9, club10,
+                           club11, club12, club13, club14, club15, club16, club17, club18, club19, club20 };
     for (auto club : clubs) {
         premier_league.addclub(club);
     }
-    premier_league.displayLeague();
+    //premier_league.displayLeague();
+    premier_league.addmatchday();
+    premier_league.addmatchday();
+    premier_league.addmatchday();
+    premier_league.displayMatchday();
 }
